8da7b5cf56938f6746ae11678664db38c8abdc19
Picsou <bpicsou@live.fr>
2019-11-05 10:24:08 +0100

    Mon application de blog

---
+++ blogs/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+    path('post/new/$', views.post_new, name='post_new'),
+    path('post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+    path('post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name="post_edit"),
+]
